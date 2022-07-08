45707d78f9b90339b4825b0575c965284c3be0de
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-08 16:18:20 -0700

    Add display_board function

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,16 +1,19 @@
+require 'pp'
 # A Piece object will be created to fill in a spot in the Board each turn
-# Colors are ⚪ and ⚫
+# Checkers are X and O
 class Piece
-  attr_reader :color
+  attr_reader :checker
 
-  def initialize(color)
-    @color = color
+  def initialize(checker)
+    @checker = checker
   end
 end
-
+# The game will be played over the Board object, which will hold Pieces
+# I am currently using a nested hash for the board but this could change
 class Board
   attr_accessor :board
-
+  # Each row is a number 1-6
+  # Each column is a letter a-g
   def initialize
     @board = {
       6 => {a: "-", b: "-", c: "-", d: "-", e: "-", f: "-", g: "-"},
@@ -21,4 +24,18 @@ class Board
       1 => {a: "-", b: "-", c: "-", d: "-", e: "-", f: "-", g: "-"}
     }
   end
+  # Displays the board in a presentable way
+  def display_board
+    puts "       CONNECT FOUR"
+
+    @board.each do |k, v|
+      puts "| #{v[:a]} | #{v[:b]} | #{v[:c]} | #{v[:d]} | #{v[:e]} | #{v[:f]} | #{v[:g]} |"
+      puts "☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩☩"
+    end
+
+    puts "  A ☩ B ☩ C ☩ D ☩ E ☩ F ☩ G"
+  end
 end
+
+board = Board.new
+board.display_board

16e03452ba8671bed04e1e6743fe2caae2449783
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-07-09 18:03:53 -0700

    Remove Piece class

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,12 +1,3 @@
-# A Piece object will be created to fill in a spot in the Board each turn
-# Checkers are X and O
-class Piece
-  attr_reader :checker
-
-  def initialize(checker)
-    @checker = checker
-  end
-end
 # The game will be played over the Board object, which will hold Pieces
 # I am currently using a nested hash for the board but this could change
 class Board
